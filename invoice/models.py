4356b0143551fb2c1ca68d001869bfbf8a77f446
Akshay Bhandari <akshay.b1@outlook.com>
2023-01-04 19:16:09 +0530

    quantity error and addition after deletion of invoice

--- invoice/models.py
+++ invoice/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Product(models.Model):
     product_name = models.CharField(max_length=255)
     product_price = models.FloatField(default=0)
-    product_unit = models.CharField(max_length=255)
+    product_unit = models.IntegerField(default=0)
+
     product_is_delete = models.BooleanField(default=False)
 
     def __str__(self):
